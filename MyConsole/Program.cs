b6470dabb7e841d90c78ebed30c6692a3c075ca5
= <=>
2025-03-17 04:02:40 +0100

    all

---
+++ MyConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class MyConsole
+{
+    static void Main(string[] args)
+    {
+        int c = Add2Number(10, 3);
+        Console.WriteLine($"Total: {c}");
+        var person = new { Name = "John", Age = 25 };
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+    }
+
+    private static int Add2Number(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+}
